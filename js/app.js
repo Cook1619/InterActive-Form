357a6762b06edb0173e51bb4f6e155c926951955
Cook1619 <cook6348@gmail.com>
2018-09-26 08:59:18 -0500

    added comments to the function stating what it does

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Uses the global window object to call a function which selects the first input field and autofocuses to the name input
 window.onload = function() {
   document.getElementById("name").focus();
 };
