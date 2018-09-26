54684dcbbb3ad24a4bbe81f78d07e0f6f658b397
Cook1619 <cook6348@gmail.com>
2018-09-26 11:53:17 -0500

    Decided to use jquery to familiarize myself with it, then going to come back and use vanilla js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
-//Uses the global window object to call a function which selects the first input field and autofocuses to the name input
-window.onload = function() {
-  document.getElementById("name").focus();
-};
-
-let jobTitle = document.getElementById("title")[5];
-
-function otherSelected() {
-  if (jobTitle) {
-    let inputField = document.createElement("INPUT");
-    document.body.insertBefore(inputField, jobTitle);
-    inputField.setAttribute("type", "text");
-  }
-}
-otherSelected();
+// Selects by id name, and uses the Jquery method FOCUS
+$("#name").focus();
