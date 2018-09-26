726b61ad1a39170f33ce85fe50f1fb5e68d71e19
Cook1619 <cook6348@gmail.com>
2018-09-26 12:31:06 -0500

    pushing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 // Selects by id name, and uses the Jquery method FOCUS
 $("#name").focus();
+
+//Job Role
+let $div = $("<div></div>");
+let $textarea = $('<textarea style="padding-left:100px"/>');
